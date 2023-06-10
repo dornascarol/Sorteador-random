d78e3234c44ef867b3efb48767267bde38026abd
dornascarol <carolinafdornas@gmail.com>
2023-06-10 15:02:49 -0300

    adicionando operadores de comparacao nos valores de minino e maximo

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 function generateNumber() {
-    
     const minNumber = Math.ceil(document.querySelector(".input-min").value)
     const maxNumber = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    if (minNumber >= maxNumber) {
+        alert("O valor mínimo tem que ser menor do que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber
 
-    alert(result)
+        alert(result)
+    }
 }
